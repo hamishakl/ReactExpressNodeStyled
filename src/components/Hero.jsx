e3ff9b9fb8d99fe5c83a1646a1fbe9e95e6ffbef
hamishakl <hamish.tana24@gmail.com>
2020-10-03 20:40:59 +1300

    created component folder and basic ui components

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Hero extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
