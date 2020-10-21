1ea8ac217c767db386a11efc8b44a9714b807969
hamishakl <hamish.tana24@gmail.com>
2020-10-22 09:13:06 +1300

    navbar finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react"
+import Navbar from "./components/Navbar"
 
 export class App extends Component {
   render() {
     return (
       <>
-        asda
+        <Navbar />
       </>
     )
   }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,28 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
+import React, { Component } from "react"
+import styled from "styled-components"
+
+const Title = styled.h5`
+`
+
+const NavWrapper = styled.div`
+  width: 100%;
+  height: 8vh;
+  color: white;
+  background-color: black;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`
 
 export default class Navbar extends Component {
-    render() {
-        return (
-            <div>
-                Navbar
-            </div>
-        )
-    }
+  render() {
+    return (
+      <>
+      <NavWrapper>
+        <Title>React Styled Node</Title>
+      </NavWrapper>
+      </>
+    )
+  }
 }
